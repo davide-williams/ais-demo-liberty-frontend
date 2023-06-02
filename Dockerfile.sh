9a26a83b4a15de61136a9d0b13a520baee9a490b
davide-williams <DavidE.Williams@vita.virginia.gov>
2023-06-02 17:27:26 -0400

    update

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -1,2 +1,15 @@
-Hello
-dss
+echo ' FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi as builder
+
+FROM builder as build1
+
+COPY --chown=1001:0 /config/server.xml /config/server.xml
+
+#RUN features.sh
+
+COPY --chown=1001:0 /cert/key.p12 /config/resources/security/key.p12
+
+COPY --chown=1001:0 '$APP_BUILD_PATH/$APP_NAME'.war /config/apps/
+
+COPY --chown=1001:0 /cert/keystore.xml /config/configDropins/defaults/keystore.xml 
+
+RUN configure.sh ' > Dockerfile
