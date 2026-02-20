c3984b851f71149b3440dc275ac821582b2e0789
davide-williams <DavidE.Williams@vita.virginia.gov>
2026-02-20 17:11:02 -0500

    Update Junit test case

--- front-end-service/src/test/java/gov/ais/bes/test/BESExampleTest.java
+++ front-end-service/src/test/java/gov/ais/bes/test/BESExampleTest.java
@@ -1,12 +1,18 @@
 package gov.ais.bes.test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+import gov.ais.bes.model.Applicant;
 
 public class BESExampleTest {
-	
+
 	@Test
-	public void justAnExample() {
-		
+	public void applicanApprovalStatus() {
+		Applicant applicant = new Applicant();
+
+		// Assert
+		assertTrue("Applicant is Approved", applicant.isApproved());
 	}
 
 }
